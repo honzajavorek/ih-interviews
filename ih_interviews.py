b46e0a03a952f7a449e1cfe268f617d2783e1e41
Honza Javorek <mail@honzajavorek.cz>
2020-11-23 21:19:39 +0100

    initial implementation

---
+++ ih_interviews.py
@@ -0,0 +1,26 @@
+import os
+from datetime import datetime
+
+import pytz
+from algoliasearch.search_client import SearchClient
+from feedgen.feed import FeedGenerator
+
+
+if __name__ == '__main__':
+    client = SearchClient.create(os.environ['APP_ID'], os.environ['API_KEY'])
+    index = client.init_index('interviews_publishedAt_desc')
+    articles = index.search('')['hits']
+
+    fg = FeedGenerator()
+    fg.title('IH Interviews')
+    fg.id('ih-interviews-20201123-205642')
+    for article in articles:
+        pub = datetime.fromtimestamp(article['publishedAt'] / 1000).replace(tzinfo=pytz.timezone('UTC'))
+        fe = fg.add_entry()
+        fe.id(article['interviewId'])
+        fe.published(pub)
+        fe.pubdate(pub)
+        fe.updated(pub)
+        fe.title(article['title'])
+        fe.link(href=f"https://www.indiehackers.com/interview/{article['interviewId']}")
+    print(fg.atom_str(pretty=True).decode())
